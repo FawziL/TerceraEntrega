021b94934f23c616f9bdb72fe24bf8ffaaeabf63
FawziLutfallah <fawzi@gmail.com>
2022-10-19 22:20:09 -0400

    chia, supertest y mocha

--- models/producto.js
+++ models/producto.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    nombre:{type:String,required: true,},
-    foto:{type:String,required: true,},
-    precio:{type:Number,required: true,},
-    descripcion:{type:Number,required: true,},
-    codigo:{type:Number,required: true,},
+    title:{type:String,required: true,},
+    thumbnail:{type:String,required: true,},
+    stock:{type:Number,required: true,},
+    price:{type:Number,required: true,},
+    description:{type:Number,required: true,},
+    code:{type:String,required: true,},
 });
 
 
 const productModel = mongoose.model('Product',productSchema);
 
 module.exports = productModel
-
