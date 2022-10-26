26cc882c812bfb5ffd533caf13cb0bdd584c37ff
FawziLutfallah <fawzi@gmail.com>
2022-10-25 23:44:36 -0400

    arreglado el modelo de productos y plantillas producto-cart

--- models/producto.js
+++ models/producto.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     title:{type:String,required: true,},
     thumbnail:{type:String,required: true,},
-    stock:{type:Number,required: true,},
     price:{type:Number,required: true,},
     description:{type:Number,required: true,},
-    code:{type:String,required: true,},
+    code:{type:Number,required: true,},
+    stock:{type:Number,required: true,},
 });
 
 
 const productModel = mongoose.model('Product',productSchema);
 
 module.exports = productModel
+
